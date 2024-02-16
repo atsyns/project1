bac8eb4c431814efb96e438fd09105334cfb8e59
atsyns <ats.yns@gmail.com>
2024-02-17 00:37:37 +0700

    new

---
+++ asset/layout/header.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html data-theme="night">
+
+
+<head>
+    <base href="http://localhost/www/project1/">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans+Thai:wght@400;600&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link href="./asset/tailwind/output.css" rel="stylesheet">
+    <link href="./asset/css/custom.css" rel="stylesheet">
+
+    <!-- CDN -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.21/css/jquery.dataTables.min.css" />
+    <!-- <link href="https://cdn.jsdelivr.net/npm/daisyui@4.6.1/dist/full.min.css" rel="stylesheet" type="text/css" />
+    <script src="https://cdn.tailwindcss.com"></script> -->
+
+</head>
+
+<body style="font-family: 'IBM Plex Sans Thai', sans-serif;" class="">
---
+++ index.php
@@ -0,0 +1,46 @@
+<?php include('asset/layout/header.php') ?>
+<?php include('asset/layout/navbar.php') ?>
+
+
+<div class="container mx-auto mt-12">
+    <div class="text-center pb-12 md:pb-16">
+        <h1 class="text-5xl md:text-6xl font-extrabold  tracking-wide mb-4">KING <span class="bg-gradient-to-r from-orange-700 to-amber-400 bg-clip-text text-transparent">BURGER</span></h1>
+        <div class="max-w-3xl mx-auto">
+            <p class="text-xl text-base-content mb-8">เบอร์เกอร์เนื้อวัว 100% อบสดใหม่ทุกชิ้น หอมชีสเยิ้มๆ ทานคู่กับเฟรนช์ฟรายส์และโค้กเย็นๆ ฟินสุดๆ!</p>
+            <div class="flex justify-between">
+                <ul class="text-lg text-left">
+                    <li class="ml-2">Beef</li>
+                    <li class="ml-2">Pork</li>
+                    <li class="ml-2">Chicken</li>
+                </ul>
+                <ul class="text-lg hidden md:block">
+                    <li class="mr-2">....</li>
+                    <li class="mr-2">....</li>
+                    <li class="mr-2">....</li>
+                </ul>
+                <ul class="text-lg text-right">
+                    <li class="mr-2">Beef</li>
+                    <li class="mr-2">Pork</li>
+                    <li class="mr-2">Chicken</li>
+                </ul>
+            </div>
+
+            <div class="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center mt-10">
+                <div><a class="btn text-white bg-blue-600 hover:bg-blue-700 w-full mb-4 sm:w-auto sm:mb-0" href="#0">Start free trial</a></div>
+                <div><a class="btn text-white bg-gray-900 hover:bg-gray-800 w-full sm:w-auto sm:ml-4" href="#0">Learn more</a></div>
+            </div>
+        </div>
+    </div>
+
+</div>
+<div class="container mt-12 px-12">
+    <div class="h-24">
+        <img class="float-left gap-4" style="height: 50%;" src="https://cdn.pixabay.com/photo/2020/10/05/19/55/hamburger-5630646_1280.jpg">
+        <p class="">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corporis, at qui incidunt fuga accusamus laboriosam commodi placeat rerum aliquid, velit molestiae! Amet earum
+            facilis quaerat sint
+            aliquam et, nihil in ducimus commodi libero doloribus tenetur placeat consequatur provident quasi architecto laboriosam voluptates cumque voluptatum. Officia odit modi quidem qui
+            voluptatibus.
+        </p>
+    </div>
+    <h1>1</h1>
+</div>
